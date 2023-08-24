ea16b41afc0eb6bcd4adcd1482182afdac7cbaaa
lucasmessias322 <lucasmessiaspereira322@gmail.com>
2023-08-24 18:12:49 -0300

    pwa

---
+++ public/service-worker.js
@@ -0,0 +1,35 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("tabuada-cache").then((cache) => {
+      return cache.addAll([
+        "/", // Add the paths to your assets
+        "/index.html",
+        // ...add other paths
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // If the resource is already cached, return it
+      if (response) {
+        return response;
+      }
+
+      // If the resource is not cached, fetch it and cache it
+      return fetch(event.request).then((response) => {
+        // Clone the response to use it for caching and for serving to the browser
+        const clonedResponse = response.clone();
+
+        // Open a cache and put the cloned response in it
+        caches.open("tabuada-cache").then((cache) => {
+          cache.put(event.request, clonedResponse);
+        });
+
+        return response;
+      });
+    })
+  );
+});
--- src/index.js
+++ src/index.js
@@ -9,3 +9,16 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log("Service Worker registered with scope:", registration.scope);
+      })
+      .catch((error) => {
+        console.log("Service Worker registration failed:", error);
+      });
+  });
+}
