c9041ea80f5c650243be8f1c56e490512cb03059
lucasmessias322 <lucasmessiaspereira322@gmail.com>
2023-10-25 17:20:17 -0300

    service-worker update

--- public/service-worker.js
+++ public/service-worker.js
@@ -26,29 +26,55 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      // If the resource is already cached, return it
-      if (response) {
-        return response;
-      }
-
-      // If the resource is not cached, try to fetch it
-      return fetch(event.request)
-        .then((networkResponse) => {
-          // Clone the response to use it for caching and for serving to the browser
-          const clonedResponse = networkResponse.clone();
-
-          // Open the cache and put the cloned response in it
-          caches.open(cacheName).then((cache) => {
-            cache.put(event.request, clonedResponse);
-          });
-
-          return networkResponse;
-        })
-        .catch(() => {
-          // If fetch fails and there's no cache, return a fallback response
-          return caches.match("/offline.html"); // Provide an offline fallback page
+    // Check if the user is online
+    fetch(event.request)
+      .then((networkResponse) => {
+        // If there's a network response, update the cache and return the network response
+        const clonedResponse = networkResponse.clone();
+        caches.open(cacheName).then((cache) => {
+          cache.put(event.request, clonedResponse);
         });
-    })
+        return networkResponse;
+      })
+      .catch(() => {
+        // If there's no network response, try to serve from cache
+        return caches.match(event.request).then((cacheResponse) => {
+          if (cacheResponse) {
+            return cacheResponse;
+          } else {
+            // If not found in cache, return a fallback response
+            return caches.match("/offline.html"); // Provide an offline fallback page
+          }
+        });
+      })
   );
 });
+
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then((response) => {
+//       // If the resource is already cached, return it
+//       if (response) {
+//         return response;
+//       }
+
+//       // If the resource is not cached, try to fetch it
+//       return fetch(event.request)
+//         .then((networkResponse) => {
+//           // Clone the response to use it for caching and for serving to the browser
+//           const clonedResponse = networkResponse.clone();
+
+//           // Open the cache and put the cloned response in it
+//           caches.open(cacheName).then((cache) => {
+//             cache.put(event.request, clonedResponse);
+//           });
+
+//           return networkResponse;
+//         })
+//         .catch(() => {
+//           // If fetch fails and there's no cache, return a fallback response
+//           return caches.match("/offline.html"); // Provide an offline fallback page
+//         });
+//     })
+//   );
+// });
