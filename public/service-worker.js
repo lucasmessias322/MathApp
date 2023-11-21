17586b00154e33c4e8f909c6ed5adff96005d9b0
lucasmessias322 <lucasmessiaspereira322@gmail.com>
2023-11-21 12:27:28 -0300

    update service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,12 @@
-const cacheName = "tabuada-cache-v8"; // Update version when you change assets
+const cacheName = "tabuada-cache-v9"; // Atualize a versão ao alterar os ativos
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(cacheName).then((cache) => {
-      return cache.addAll([
-        "/", // Add the paths to your assets
-        "/index.html",
-        // ...add other paths
-      ]);
+      return fetch("/api/getAllUrls") // Endpoint para obter todas as URLs do seu aplicativo
+        .then((response) => response.json())
+        .then((urls) => cache.addAll(urls))
+        .catch((error) => console.error("Error fetching URLs:", error));
     })
   );
 });
@@ -26,29 +25,82 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    // Check if the user is online
     fetch(event.request)
       .then((networkResponse) => {
-        // If there's a network response, update the cache and return the network response
-        const clonedResponse = networkResponse.clone();
-        caches.open(cacheName).then((cache) => {
-          cache.put(event.request, clonedResponse);
-        });
+        // Se houver uma resposta da rede, atualize o cache e retorne a resposta da rede
+        if (networkResponse.status === 200) {
+          const clonedResponse = networkResponse.clone();
+          caches.open(cacheName).then((cache) => {
+            cache.put(event.request, clonedResponse);
+          });
+        }
         return networkResponse;
       })
       .catch(() => {
-        // If there's no network response, try to serve from cache
+        // Se não houver resposta da rede, tente servir a partir do cache
         return caches.match(event.request).then((cacheResponse) => {
-          if (cacheResponse) {
+          if (cacheResponse && cacheResponse.status === 200) {
             return cacheResponse;
           } else {
-            // If not found in cache, return a fallback response
-            return caches.match("/offline.html"); // Provide an offline fallback page
+            // Se não encontrado no cache ou não for uma resposta bem-sucedida, retorne uma resposta de fallback
+            return caches.match("/offline.html"); // Forneça uma página de fallback offline
           }
         });
       })
   );
 });
+
+// const cacheName = "tabuada-cache-v8"; // Update version when you change assets
+
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(
+//     caches.open(cacheName).then((cache) => {
+//       return cache.addAll([
+//         "/", // Add the paths to your assets
+//         "/index.html",
+//         // ...add other paths
+//       ]);
+//     })
+//   );
+// });
+
+// self.addEventListener("activate", (event) => {
+//   event.waitUntil(
+//     caches.keys().then((cacheNames) => {
+//       return Promise.all(
+//         cacheNames
+//           .filter((name) => name !== cacheName)
+//           .map((name) => caches.delete(name))
+//       );
+//     })
+//   );
+// });
+
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     // Check if the user is online
+//     fetch(event.request)
+//       .then((networkResponse) => {
+//         // If there's a network response, update the cache and return the network response
+//         const clonedResponse = networkResponse.clone();
+//         caches.open(cacheName).then((cache) => {
+//           cache.put(event.request, clonedResponse);
+//         });
+//         return networkResponse;
+//       })
+//       .catch(() => {
+//         // If there's no network response, try to serve from cache
+//         return caches.match(event.request).then((cacheResponse) => {
+//           if (cacheResponse) {
+//             return cacheResponse;
+//           } else {
+//             // If not found in cache, return a fallback response
+//             return caches.match("/offline.html"); // Provide an offline fallback page
+//           }
+//         });
+//       })
+//   );
+// });
 
 // self.addEventListener("fetch", (event) => {
 //   event.respondWith(
